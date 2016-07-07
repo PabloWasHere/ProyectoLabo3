702cced965c3feda3ae68671063a88ab74323ddf
PabloWasHere <pablobarco@outlook.com.ar>
2016-07-07 14:38:00 -0300

    Arreglado problema de Classes

--- Classes/Properties/AssemblyInfo.cs
+++ Classes/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("Classes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyProduct("Classes")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d814ab4a-95b8-41f4-9a38-1de1484783f3")]
+[assembly: Guid("ced237c2-7b74-463e-8470-464f293b35ff")]
 
 // Version information for an assembly consists of the following four values:
 //
